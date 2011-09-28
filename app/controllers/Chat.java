a04a0e6146dde0427f7bd2a93f87cf3cb9b6fd19
Amy Boyd <amy@amyboyd.co.uk>
2011-09-28 15:44:58 +0200

    Removed web socket code (not being used)

--- app/controllers/Chat.java
+++ app/controllers/Chat.java
@@ -3,10 +3,8 @@ package controllers;
 import java.util.*;
 import models.ChatRoom.Event;
 import models.*;
-import play.mvc.*;
 import play.mvc.Http.*;
 import play.libs.F.*;
-import static play.libs.F.Matcher.*;
 
 public class Chat extends BaseController {
     /**
@@ -31,38 +29,6 @@ public class Chat extends BaseController {
             final List<IndexedEvent<Event>> messages = await(getChatRoomByOtherUserId(userId).
                     nextMessages(lastReceived.longValue()));
             renderJSON(ChatRoom.toJsonObject(messages).toString());
-        }
-    }
-
-    public static class WebSocket extends WebSocketController {
-        public static void join(Long userId) {
-            final User user = requireAuthenticatedUser();
-            final ChatRoom chatRoom = getChatRoomByOtherUserId(userId);
-
-            // Socket connected, join the chat room.
-            final EventStream<ChatRoom.Event> roomStream = chatRoom.getEventStream();
-
-            while (inbound.isOpen()) {
-                // Wait for an event, either something coming on the inbound socket channel, or ChatRoom messages.
-                Either<WebSocketEvent, ChatRoom.Event> e = await(
-                        Promise.waitEither(inbound.nextEvent(), roomStream.nextEvent()));
-
-                // Case: TextEvent received on the socket
-                for (String userMessage: WebSocketEvent.TextFrame.match(e._1)) {
-                    chatRoom.say(user, userMessage);
-                }
-
-                // Case: New message on the chat room
-                for (ChatRoom.Message message: ClassOf(ChatRoom.Message.class).match(e._2)) {
-                    outbound.sendJson(message.toJsonObject());
-                }
-
-                // Case: The socket has been closed
-                // A Play bug is causing the compiler to break here.
-                for (WebSocketClose close: WebSocketEvent.SocketClosed.match(e._1)) {
-                    disconnect();
-                }
-            }
         }
     }
 }
